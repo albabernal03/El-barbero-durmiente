818705c98b03aae151a4ea1f0676b080b2fd6b30
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 11:07:19 +0200

    clase cliente terminado pasamos a la del barbero

--- porueba.py
+++ porueba.py
@@ -43,7 +43,16 @@ class Barberia:
             print(f'El cliente {nombre_cliente} se fue porque no habia sillas disponibles')
             self.mutex.release() #el cliente libera el mutex para que otro cliente pueda entrar a la barberia
 
-            
+class Cliente(threading.Thread): #clase cliente, de tipo thread
+    def __init__(self, nombre_cliente, barberia):
+        threading.Thread.__init__(self)
+        self.nombre_cliente = nombre_cliente
+        self.barberia = barberia
+
+    def run(self): #metodo run, que se ejecuta cuando se llama al metodo start, esto se usa para que el cliente entre a la barberia
+        self.barberia.nuevo_cliente(self.nombre_cliente)
+        
+
 
 
 
