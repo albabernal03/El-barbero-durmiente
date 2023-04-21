00934773afadeab7cd49f63f135401d5a01d7979
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 11:08:29 +0200

    terminamos con la clase barbero

--- porueba.py
+++ porueba.py
@@ -51,6 +51,15 @@ class Cliente(threading.Thread): #clase cliente, de tipo thread
 
     def run(self): #metodo run, que se ejecuta cuando se llama al metodo start, esto se usa para que el cliente entre a la barberia
         self.barberia.nuevo_cliente(self.nombre_cliente)
+
+class Barbero(threading.Thread): #clase barbero, de tipo thread
+    def __init__(self, barberia):
+        threading.Thread.__init__(self)
+        self.barberia = barberia
+
+    def run(self): #metodo run, que se ejecuta cuando se llama al metodo start, esto se usa para que el barbero entre a la barberia
+        self.barberia.esperar_cliente()
+
         
 
 
