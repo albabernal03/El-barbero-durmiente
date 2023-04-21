601428d99645025e821e02bedbe96e2ef04acb89
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 10:37:40 +0200

    creamos la funcion cortar el pelo

--- porueba.py
+++ porueba.py
@@ -13,4 +13,12 @@ class Barberia:
         self.barbero = threading.Semaphore(0) #semaforo para el barbero, esto es para que el barbero no atienda a mas de 1 cliente a la vez
         self.barbero_dormido= True
 
-        
+    def cortar_pelo(self):
+        print("El barbero esta cortando el pelo")
+        time.sleep(random.randit(1,5)) #tiempo que tarda en cortar el pelo
+        print("El barbero termino de cortar el pelo")
+        self.barbero.release() #libera al barbero para que pueda atender a otro cliente
+
+    def esperar_nuevo_cliente(self):
+        print("El barbero esta durmiendo")
+        self.cliente.acquire() #el acquires es para que el barbero no atienda a mas de 1 cliente a la vez
