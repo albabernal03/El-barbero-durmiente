c45e1c2fc8d1e78630d8a28fd2326074de103457
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 10:34:29 +0200

    definimos el constructor de la barberia

--- porueba.py
+++ porueba.py
@@ -6,6 +6,11 @@ import random
 
 
 class Barberia:
-    def __init__(self,num_sillas)
+    def __init__(self,num_sillas):
+        self.num_sillas = num_sillas
+        self.mutex= threading.Lock() #semaforo para la barberia, esto es para que no entren mas de 1 cliente a la vez
+        self.cliente = threading.Semaphore(0) #semaforo para el cliente, esto es para que el barbero no atienda a mas de 1 cliente a la vez
+        self.barbero = threading.Semaphore(0) #semaforo para el barbero, esto es para que el barbero no atienda a mas de 1 cliente a la vez
+        self.barbero_dormido= True
 
-
+        
