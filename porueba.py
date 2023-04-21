131a096af5fc8fe16402a07413a3afc28f217549
albabernal03 <ALBABERO@MYUAX.COM>
2023-04-21 11:11:14 +0200

    finalizamos

--- porueba.py
+++ porueba.py
@@ -61,6 +61,29 @@ class Barbero(threading.Thread): #clase barbero, de tipo thread
         self.barberia.esperar_cliente()
 
 def menu():
+    numero_sillas = int(input("Ingrese el numero de sillas disponibles en la barberia: "))
+    barberia = Barberia(numero_sillas)
+
+    while True:
+        print("1. Agregar cliente")
+        print("2. Salir")
+        opcion = int(input("Ingrese una opcion: "))
+        
+        if opcion == 1:
+            nombre_cliente = input("Ingrese el nombre del cliente: ")
+            cliente = Cliente(nombre_cliente, barberia)
+            cliente.start()
+
+        elif opcion == 2:
+            break
+        else:
+            print("Opcion invalida")
+    
+    print('Saliendo del programa...')
+    time.sleep(2)
+
+if __name__ == '__main__':
+    menu()
 
 
 
